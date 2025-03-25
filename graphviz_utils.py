fc364decf84a67d2f6c66d48d492a31d8f78fd94
Javilejo <pra21486@uvg.edu.gt>
2025-03-25 17:25:48 -0600

    [ADD] works with tokens + * ) (

--- graphviz_utils.py
+++ graphviz_utils.py
@@ -1,4 +1,5 @@
 from graphviz import Digraph
+from shuntingyard import *
 from estructuras import *
 def generate_expression_tree_image(root, filename):
     dot = Digraph(format='png')
@@ -22,3 +23,9 @@ def generate_expression_tree_image(root, filename):
 
     add_nodes_edges(root)
     dot.render(filename, format='png', cleanup=True)
+
+# generar arbol de esta postfix '+''*'|'('|')'|
+infix = "(((A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z))(((A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z))|((0|1|2|3|4|5|6|7|8|9)))*|'+'|'*'|'('|')')#"
+postfix_expr = convert_infix_to_postfix(infix)
+ast_root = build_expression_tree(postfix_expr)
+generate_expression_tree_image(ast_root, "ast_tree")
